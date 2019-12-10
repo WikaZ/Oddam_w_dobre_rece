498e590bfd8709030bb01015e6a666bf03f977d0
Wiktoria <viktoria.zavadzka@gmail.com>
2019-12-10 02:37:18 +0100

    basic structure for homeIconPath added

--- donate_your_stuff/src/components/homeIconPath.js
+++ donate_your_stuff/src/components/homeIconPath.js
@@ -1,20 +1,52 @@
 import React from 'react';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import img from "../assets/Decoration.svg";
 
-class HomeIconPath extends React.Component{
+class HomeIconPath extends React.Component {
     constructor(props) {
         super(props);
-        this.state={
-
-        }
+        this.state = {}
     }
 
     render() {
-        return(
-            <div id={"homeIconPath"} style={{height:"350px", backgroundColor:"green"}}>
-                <p>Hi, Im HomeIconPath</p>
+        return (
+            <div id={"homeIconPath"} style={{height: "350px", backgroundColor: "green"}}>
+                <Container fluid className={"containerStyle"}>
+                    <Row>
+                        <Col>
+                            <div className={"homeIconPathHeader"}>
+                                <h1>Wystarczą 4 proste kroki</h1>
+                                <img src={img} alt="decoration" />
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={2} md={2} xs={4}><h1></h1>
+
+                        </Col>
+                        <Col lg={2} md={2} xs={4}><h1>Title</h1>
+                            <p>Lorem ipsum dolor sit.</p>
+                        </Col>
+                        <Col lg={2} md={2} xs={4}><h1>Title</h1>
+                            <p>Lorem ipsum dolor sit.</p>
+                        </Col>
+                        <Col lg={2} md={2} xs={4}><h1>Title</h1>
+                            <p>Lorem ipsum dolor sit.</p>
+                        </Col>
+                        <Col lg={2} md={2} xs={4}><h1>Title</h1>
+                            <p>Lorem ipsum dolor sit.</p>
+                        </Col>
+                        <Col lg={2} md={2} xs={4}><h1></h1>
+
+                        </Col>
+                    </Row>
+                </Container>
             </div>
 
         )
     }
 }
+
 export default HomeIconPath
